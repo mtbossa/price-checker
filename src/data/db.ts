194e8913948ef4e9bf38ea10df8356cdd5eba699
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-06-27 14:46:33 -0300

    Upadte db

--- src/data/db.ts
+++ src/data/db.ts
@@ -3,12 +3,14 @@ import sqlite3 from "sqlite3";
 const db = new sqlite3.Database(":memory:");
 
 db.serialize(() => {
-    db.run("CREATE TABLE stores (name TEXT)");
+    db.run("CREATE TABLE stores (name TEXT UNIQUE)");
     db.run("INSERT INTO stores VALUES ('Pichau')");
     db.run("INSERT INTO stores VALUES ('Kabum')");
     db.run("INSERT INTO stores VALUES ('Terabyte')");
 
-    db.run("CREATE TABLE products (name TEXT, price REAL, url TEXT, store_id INTEGER)");
+    db.run(
+        "CREATE TABLE products (name TEXT UNIQUE, price REAL, url TEXT UNIQUE, store_id INTEGER NOT NULL, FOREIGN KEY (store_id) REFERENCES stores (rowid))"
+    );
 });
 
 export default db;
