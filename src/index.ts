c1af133470c7b2b85564ce8362f5ac5dfee3c50e
Mateus Ribeiro Bossa <mateus.rbossa@gmail.com>
2023-06-27 10:00:16 -0300

    Remove files

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
-/* eslint @typescript-eslint/no-floating-promises: 0 */
-import prompts from "prompts";
-
-import { buyURLHandler } from "./core/cli/handlers/buy-url.handler";
 import { print_program_name } from "./helpers/program_name";
-import { buyMethodPrompt } from "@core/cli/prompts/buy-method";
-import { buyMethodHandler } from "@core/cli/handlers/buy-method.handler";
-import { liveURLPrompt } from "@core/cli/prompts/live-url";
-import { liveURLHandler } from "@core/cli/handlers/live-url.handler";
 
 (async () => {
     print_program_name();
